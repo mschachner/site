c86ca092fcc05319c8e20b673c1e638ed5ddaf75
Mark Schachner <markj.schachner@gmail.com>
2025-09-24 14:45:11 -0400

    Restructuring project folder and fixing game scrolliness

--- assets/js/header.js
+++ assets/js/header.js
@@ -4,15 +4,15 @@ let appHeader = `
         <ul>
           <li><a class="navbar fancy-underline" href="index.html">
             Home</a></li>
-          <li><a class= "navbar fancy-underline" href="pages/research.html">
+          <li><a class= "navbar fancy-underline" href="research.html">
             Research</a></li>
-          <li><a class= "navbar fancy-underline" href="pages/teaching_outreach.html">
+          <li><a class= "navbar fancy-underline" href="teaching_outreach.html">
             Teaching & Outreach</a></li>
-          <li><a class= "navbar fancy-underline" href="pages/logsem.html">
+          <li><a class= "navbar fancy-underline" href="logsem.html">
             Logic student seminar </a></li>
-          <li><a class= "navbar fancy-underline" href="pages/movienight.html">
+          <li><a class= "navbar fancy-underline" href="movienight.html">
             Movie night</a></li>
-          <li><a class= "navbar fancy-underline" href="pages/milestone.html">
+          <li><a class= "navbar fancy-underline" href="milestone.html">
             Milestone</a></li>
         </ul>
         </nav>
